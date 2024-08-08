ade477ee1b6f015ada0e615aab4f9ec36f712a2f
guyettinger <guy@flaire.co>
2024-08-07 22:52:34 -0400

    shader background

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { ReactNode } from "react";
+import { Color } from "three";
+import { RootState } from "@react-three/fiber";
 import styled from "styled-components";
 import { AppStyleProvider } from "@/styles/AppStyleProvider";
-import { CanvasBackground, Stars } from "gle-r3f-components";
+import { PortalMaterial, ShaderCanvas } from "gle-r3f-components";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 
@@ -16,12 +18,28 @@ const PageMain = styled.main`
     flex: 1;
 `
 
+const ShaderCanvasContainer = styled.div`
+    position: fixed !important;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+`
+
 export const PageLayout = ({children}: { children: ReactNode }) => {
+    const shaderMaterial = new PortalMaterial()
+    shaderMaterial.uniforms.uColorStart.value = new Color('#353535')
+    shaderMaterial.uniforms.uColorEnd.value = new Color('#323232')
+    const renderCallback = (state: RootState, delta: number) => {
+        shaderMaterial.uniforms.uTime.value += delta
+    }
+
     return (
         <PageBody>
-            <CanvasBackground background={"#2a2a2a"}>
-                <Stars pointColor={"#131313"}/>
-            </CanvasBackground>
+            <ShaderCanvasContainer>
+                <ShaderCanvas shaderMaterial={shaderMaterial} renderCallback={renderCallback}/>
+            </ShaderCanvasContainer>
             <AppStyleProvider>
                 <Navbar/>
                 <PageMain>
