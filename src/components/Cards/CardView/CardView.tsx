ff40d54a822bd7047b71dfdc7224be07692a7d61
guyettinger <guy@flaire.co>
2025-09-25 21:20:09 -0400

    migrating to tailwind/shadcn

--- src/components/Cards/CardView/CardView.tsx
+++ src/components/Cards/CardView/CardView.tsx
@@ -1,7 +1,7 @@
 import { CardViewProps } from '@/components/Cards/CardView/CardView.types';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { cn } from '@/lib/utils'
+import { cn } from '@/lib/utils';
 
 export const CardView = ({
   title,
@@ -13,7 +13,7 @@ export const CardView = ({
   ...cardProps
 }: CardViewProps) => {
   return (
-    <Card className={cn("flex cursor-pointer flex-col", className)} {...cardProps}>
+    <Card className={cn('flex cursor-pointer flex-col', className)} {...cardProps}>
       <CardHeader className="gap-2 flex-row justify-between items-center">
         {!!iconArea && <div className="flex h-4 w-4 justify-center">{iconArea}</div>}
         {!!title && (
@@ -32,7 +32,7 @@ export const CardView = ({
 
       {!!children && (
         <div className="flex flex-1 w-full justify-center">
-          <ScrollArea type="hover" className="flex-1 w-full max-h-[70vh]">
+          <ScrollArea type="hover" className="flex-1 w-full max-h-[70vh] rounded-b-xl bg-[rgb(18,18,18)]">
             <CardContent className="py-4">{children}</CardContent>
           </ScrollArea>
         </div>
